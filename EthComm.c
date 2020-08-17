2e4589e695e8587a2f1472817da6ecb48d878ba9
wangj <jian.wang@du-power.com>
2020-08-17 20:16:14 +0800

    在Master分支上添加了Ethernet时间计数静态全局变量

--- EthComm.c
+++ EthComm.c
@@ -29,6 +29,7 @@
 #include "rba_EthTcp_Prv.h"
 #include "rba_EthTcp_Prv_Domain.h"
 
+static int ethernet_timeCount = 0;
 
 zvt_pt_ipmode IpModeFlag     = User_IpMode;
 zvt_pt_ipmode PrevIpModeFlag = User_IpMode;
